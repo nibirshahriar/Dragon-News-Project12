1122b7f37608c1ffe4750de699798f9a66afdab9
nibirshahriar <nibir1891@gmail.com>
2026-05-25 08:12:41 +0600

    added not found page in app folder

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import { format } from "date-fns";
 
 const Header = () => {
-    return (
-        <div>
-            header
-        </div>
-    );
+  return (
+    <div className="text-center py-8 space-y-2">
+      <Image
+        src={logo}
+        alt="Logo"
+        width={300}
+        height={200}
+        className="mx-auto"
+      />
+      <p>Journalism Without Fear or Favour</p>
+      <p>{format(new Date(), "EEEE, MMM dd, yyyy")}</p>
+    </div>
+  );
 };
 
 export default Header;
