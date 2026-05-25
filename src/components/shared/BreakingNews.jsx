0cbc5618d3c3bbafdbf612d5780377624645a054
nibirshahriar <nibir1891@gmail.com>
2026-05-25 08:41:49 +0600

    added react-fast-marquee lib

---
+++ src/components/shared/BreakingNews.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const BreakingNews = () => {
+  return (
+    <div>
+      <Marquee>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam unde
+        deserunt facilis dolor aut excepturi dolorem! Illum eius aliquid atque
+        sunt mollitia, ullam vero dolor quibusdam dicta blanditiis, commodi,
+        repudiandae esse vitae odio eaque cupiditate laudantium natus deserunt
+        possimus facilis ducimus saepe non. Fugiat consequatur adipisci
+        exercitationem facere in reprehenderit laborum natus cum quos
+        accusantium dolorem deleniti ex, id repellendus odit accusamus
+        repudiandae? Accusantium dolorem modi eveniet ex reprehenderit debitis
+        ab, vero deleniti! Omnis, debitis possimus! Quisquam tenetur, reiciendis
+        maxime voluptatibus eaque quaerat molestiae natus magnam voluptates qui
+        modi error quo consequatur quae minima vero nulla facere? Minima, vero
+        voluptate.
+      </Marquee>
+    </div>
+  );
+};
+
+export default BreakingNews;
