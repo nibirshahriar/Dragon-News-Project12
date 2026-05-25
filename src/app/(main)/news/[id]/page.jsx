715b9732aa0417cc3139c8f07296e5dbc92cea4e
nibirshahriar <nibir1891@gmail.com>
2026-05-25 07:57:20 +0600

    added route groups for multiple pages manage

---
+++ src/app/(main)/news/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsDetailPage = () => {
+    return (
+        <div>
+            News Detail Page
+        </div>
+    );
+};
+
+export default NewsDetailPage;
---
+++ src/app/(main)/page.js
@@ -0,0 +1,8 @@
+ 
+
+export default function Home() {
+  return (
+     <h2 className="text-amber-200">Welcome to the Home Page</h2>
+
+  );
+}
